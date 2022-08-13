4ede95675bdb9010e741f0bc36e9ca6647f6cf22
amirmahdi <amirmm71382@gmail.com>
2022-08-14 01:15:51 +0430

    move of police started

--- src/main/java/ir/sharif/aic/hideandseek/ai/PoliceAI.java
+++ src/main/java/ir/sharif/aic/hideandseek/ai/PoliceAI.java
@@ -1,10 +1,19 @@
 package ir.sharif.aic.hideandseek.ai;
 
 import ir.sharif.aic.hideandseek.client.Phone;
+import ir.sharif.aic.hideandseek.protobuf.AIProto;
 import ir.sharif.aic.hideandseek.protobuf.AIProto.GameView;
+import ir.sharif.aic.hideandseek.protobuf.AIProto.Agent;
+
+import javax.swing.text.StyledEditorKit;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class PoliceAI extends AI {
-    PoliceGraphController policeGraphController;
+    private PoliceGraphController policeGraphController;
+    private ArrayList<AIProto.Agent> thiefs = new ArrayList<>();
+    private boolean hasReachedATheif = false;
+    private Random random = new Random();
 
     public PoliceAI(Phone phone) {
         this.phone = phone;
@@ -24,7 +33,41 @@ public class PoliceAI extends AI {
      */
     @Override
     public int move(GameView gameView) {
-        return 1;
+        if (gameView.getConfig().getTurnSettings().getVisibleTurnsList().contains(gameView.getTurn().getTurnNumber())) {
+            updateThief(gameView);
+            hasReachedATheif = false;
+        }
+        if (hasReachedATheif) {
+            //random move
+        }
+        if (thiefs.isEmpty()) {
+            //distribute
+        }
+        Agent me = gameView.getViewer();
+        Agent closestThief = null;
+        for (Agent thief : thiefs) {
+            if (closestThief == null || policeGraphController.getDistance(me.getNodeId(), thief.getNodeId()) <
+                    policeGraphController.getDistance(me.getNodeId(), closestThief.getNodeId()))
+                closestThief = thief;
+        }
+
+        int nextNode = policeGraphController.getNextOnPath(me.getNodeId(), closestThief.getNodeId());
+        if (nextNode == closestThief.getNodeId()){
+            thiefs.remove(closestThief);
+            hasReachedATheif = true;
+        }
+        return nextNode;
+
+    }
+
+    private void updateThief(GameView gameView) {
+        Agent me = gameView.getViewer();
+        thiefs = new ArrayList<>();
+        for (Agent agent : gameView.getVisibleAgentsList()) {
+            if (agent.getTeamValue() != me.getTeamValue() && agent.getType() == AIProto.AgentType.THIEF && !agent.getIsDead()) {
+                thiefs.add(agent);
+            }
+        }
     }
 
 }
--- src/main/java/ir/sharif/aic/hideandseek/ai/PoliceGraphController.java
+++ src/main/java/ir/sharif/aic/hideandseek/ai/PoliceGraphController.java
@@ -1,9 +1,6 @@
 package ir.sharif.aic.hideandseek.ai;
 
 import ir.sharif.aic.hideandseek.protobuf.AIProto.Graph;
-import ir.sharif.aic.hideandseek.protobuf.AIProto.Path;
-
-import java.util.ArrayList;
 
 public class PoliceGraphController extends GraphController{
 
@@ -11,4 +8,8 @@ public class PoliceGraphController extends GraphController{
     public PoliceGraphController(Graph graph) {
         super(graph);
     }
+
+
+
+
 }
